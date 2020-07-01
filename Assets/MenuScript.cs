fabb46922ee89f3f82c2ab297a0c1eb52a3bd328
Tolga Z <tolga_tr@live.nl>
2020-07-01 19:54:44 +0200

    first commit

---
+++ Assets/MenuScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+
+	public void StartGame()
+	{
+
+		Application.LoadLevel("SampleScene");
+	}
+}
